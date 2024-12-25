4319a2f61fed49e2a503ea1fbe5289d21eabe7ad
_index <indexd@pm.me>
2024-12-25 13:11:56 +0100

    draft

--- src/network/bridge.rs
+++ src/network/bridge.rs
@@ -1,1 +1,18 @@
+use crate::wireguard::ctx::WG_CTX;
+use esp_idf_svc::{eth::{EspEth, RmiiEth}, handle::RawHandle, sys::wg::wireguard_ctx_t};
 
+pub fn _start_bridge(eth_netif: EspEth<'static, RmiiEth>) -> anyhow::Result<()> {
+    let mut lock = WG_CTX.lock().unwrap();
+
+    let ctx: *mut wireguard_ctx_t = lock.as_mut().unwrap().get_raw();
+
+    unsafe {
+        let _wg_netif = ctx.as_ref().unwrap().netif;
+        let _eth_netif = eth_netif.netif().handle();
+
+        //create callbacks between both netifs to bridge them?
+
+    }
+
+    Ok(())
+}
